5be03dc11dfdaab42ecb50053ffb1a9ef4ae51ca
Abdoullah Ndao <147827751+utachicodes@users.noreply.github.com>
2025-06-04 12:19:38 +0000

    Update base path and add .nojekyll for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/andao/",
   server: {
     host: "::",
     port: 8080,
@@ -15,4 +15,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
 });
+
